069f79b85dd6839faada652c5d487f9edd44868f
Mona Lisa <m6132@yandex.ru>
2024-04-17 19:11:36 +0300

    test

--- resources/views/inc/modal/leadsmodal/add_task.blade.php
+++ resources/views/inc/modal/leadsmodal/add_task.blade.php
@@ -32,7 +32,7 @@
                      
                         <input type="hidden" name="lead_id" value="{{ $data->id }}">
                         <input type="hidden" name="lead_phone" value="{{ $data->phone }}">
-                        <input type="hidden" id="tasktype" name="type">
+                        <input type="hidden" id="tasktype" name="type" value="test">
                         <button type="submit" id='submit' class="btn btn-primary">Сохранить</button>
                     </form>
                 </div>
