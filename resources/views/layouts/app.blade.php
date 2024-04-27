3e99f04b0b35e5c840e8ad3e2b335b9f50622410
Mario <you@example.com>
2024-04-27 14:55:55 +0300

    bootstrap local

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,23 @@
 <head>
     <meta charset="utf-8">
     <title>@yield('title')</title>
+    
+    <script src="/js/jquery-3.7.1.min.js"></script>
+    <link href="/js/bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="/js/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!--bootstrap and jq composer
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-    <script type="text/javascript" src="{{ asset('/resources/js/main.js') }}"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    -->
+    <script type="text/javascript" src="{{ asset('/resources/js/main.js') }}"></script>
+    
+
+
+
     <link href="/style.css" rel="stylesheet">
     <link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
 
