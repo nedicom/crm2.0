5188922f243f9f4769961b9592178d7abc4387af
Mario <you@example.com>
2025-06-12 16:12:16 +0300

    test

--- resources/views/avito/avito_chats.blade.php
+++ resources/views/avito/avito_chats.blade.php
@@ -75,7 +75,9 @@
                 $('input[type=checkbox]').change(function() {
                     var chatId = $(this).attr('id').split('-').pop(); // получить id из id чекбокса
                     var isActive = $(this).is(':checked') ? 1 : 0;
-alert(id)
+alert(chatId)
+alert(isActive)
+alert('{{ csrf_token() }}')
                     $.ajax({
                         url: 'https://crm.nedicom.ru/update-gpt-active', // маршрут для обновления
                         type: 'POST',
